29dc4a900c0bf14e515cebbbb45f72247475368a
Tim <timjonssonn03@gmail.com>
2026-05-20 21:42:09 +0200

    Timestamps!

--- scripts/module.js
+++ scripts/module.js
@@ -5,5 +5,9 @@ Hooks.once("ready", () => {
 Hooks.once(SimpleCalendar.Hooks.Ready, () => {
   //const players = game.actors.filter(actor => actor.type === "character");
   game.settings.register("mountain-module", "time-tracker", SimpleCalendar.api.currentDateTime());
-  console.log(game.settings);
+});
+
+Hooks.on(SimpleCalendar.Hooks.DateTimeChange, (data) => {
+  const trackedDateTimeStamp = SimpleCalendar.api.dateToTimestamp(game.settings.settings.get("mountain-module.time-tracker"));
+  console.log(trackedDateTimeStamp)
 });
