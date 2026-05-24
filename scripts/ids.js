612805933bd2259442a23495429f74c6ab9d8cc1
Tim <timjonssonn03@gmail.com>
2026-05-24 02:12:20 +0200

    Small Fixes

--- scripts/ids.js
+++ scripts/ids.js
@@ -1,1 +1,1 @@
-export const bastionDefender = 'Compendium.tim-qol.tim-qol-actors.Actor.E9CvDPDg5dFEpVjS';
+export const bastionDefender = 'Compendium.tim-qol.tim-qol-actors.Actor.0fGZn2wG2vp4gKHj';
